f6ba30474462839ecc6f6fcdd31b481745149f71
David Shnayder <dusrdev@gmail.com>
2022-01-01 10:54:02 +0200

    Add project files.

---
+++ Assignment14/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Assignment14 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
